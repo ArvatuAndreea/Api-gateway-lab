a55b43dca45491852aa8903a57c7bbdfc589abf3
andreea_arvatu <arvatuandreea@yahoo.com>
2022-04-08 09:35:45 +0300

    Added server application

---
+++ ApiGatewayClient/src/test/java/com/example/apigateway/ApiGatewayClient/ApiGatewayClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigateway.ApiGatewayClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
